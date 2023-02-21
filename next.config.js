3e5d6646779189a49e86c61fc905a5ff1e56ae22
Miles <milsman2@gmail.com>
2023-02-21 00:00:47 +0000

    Fix locale.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   output: 'standalone',
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
 };
 
 module.exports = nextConfig;
